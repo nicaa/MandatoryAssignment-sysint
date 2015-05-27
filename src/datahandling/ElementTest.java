38729a84660b9ed9be9277ae3fba6f8dd5bdeb9e
Rasmus Højte <rasmusvh@msn.com>
2015-05-27 18:25:21 +0200

    Update orderItem and removeOrderItem

--- src/datahandling/ElementTest.java
+++ src/datahandling/ElementTest.java
@@ -8,6 +8,7 @@ package datahandling;
 import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
+import javax.xml.xpath.XPathExpressionException;
 import org.xml.sax.SAXException;
 
 /**
@@ -15,8 +16,10 @@ import org.xml.sax.SAXException;
  * @author Rasmus
  */
 public class ElementTest {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException, XPathExpressionException {
         XMLHandler handler = new XMLHandler();
-        handler.addNewOrderItem("Pear", "25");
+        //handler.addNewOrderItem("Pear", "25");
+        //handler.removeOrderItem("Apple");
+        handler.updateOrderItem("Banana", "125");
     }
 }
--- src/datahandling/XMLHandler.java
+++ src/datahandling/XMLHandler.java
@@ -17,6 +17,11 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -37,7 +42,7 @@ public class XMLHandler {
         Element root = document.getDocumentElement();
         NodeList nodes = root.getElementsByTagName("Order");
         
-        Node childNode = document.createElement("Item");
+        Node childNode = document.createElement("ns1:Item");
         Node productNode = document.createElement("ns1:Product");
         productNode.appendChild(document.createTextNode(productName));
         Node priceNode = document.createElement("ns1:Price");
@@ -45,7 +50,62 @@ public class XMLHandler {
         childNode.appendChild(productNode);
         childNode.appendChild(priceNode);
        // nodes.item(0).appendChild(childNode);
-        root.getElementsByTagName("ns1:Order").item(0).appendChild(childNode);
+        
+        root.getElementsByTagName("ns1:Order").item(0).insertBefore(childNode, root.getElementsByTagName("ns1:Order").item(0).getFirstChild());
+        
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        File outputFile = new File("src/XML/productOrderOutput.xml");
+        StreamResult result = new StreamResult(outputFile);
+        transformer.transform(source, result);
+    }
+    
+    public void removeOrderItem(String productName) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException, TransformerConfigurationException, TransformerException {
+        File xmlFile = new File("src/XML/productOrder.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(xmlFile);
+        Element root = document.getDocumentElement();
+        Node orderNode = root.getElementsByTagName("Order").item(0);
+        
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xpath = xpf.newXPath();
+        XPathExpression expression = xpath.compile("//Order/Item[Product='"+productName+"']");
+        
+        Node removalNode = (Node) expression.evaluate(document, XPathConstants.NODE);
+        removalNode.getParentNode().removeChild(removalNode);
+        
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        File outputFile = new File("src/XML/productOrderOutput.xml");
+        StreamResult result = new StreamResult(outputFile);
+        transformer.transform(source, result);
+    }
+    
+    public void updateOrderItem(String productName, String priceUpdate) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException, TransformerConfigurationException, TransformerException {
+        File xmlFile = new File("src/XML/productOrder.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(xmlFile);
+        Element root = document.getDocumentElement();
+        Node orderNode = root.getElementsByTagName("Order").item(0);
+        
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xpath = xpf.newXPath();
+        XPathExpression expression = xpath.compile("//Order/Item[Product='"+productName+"']");
+        
+        Node updateNode = document.createElement("ns1:Item");
+        Node productNode = document.createElement("ns1:Product");
+        productNode.appendChild(document.createTextNode(productName));
+        Node priceNode = document.createElement("ns1:Price");
+        priceNode.appendChild(document.createTextNode(priceUpdate)); 
+        updateNode.appendChild(productNode);
+        updateNode.appendChild(priceNode);
+        
+        Node replacementNode = (Node) expression.evaluate(document, XPathConstants.NODE);
+        replacementNode.getParentNode().replaceChild(updateNode, replacementNode);
         
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
